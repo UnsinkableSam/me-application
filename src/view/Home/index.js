accc97804a0bef3c5d196f8d22b3daba17ecb2cd
Unsinkablesam <maraphostv@gmail.com>
2019-09-06 17:09:02 +0100

    design improvements size and responsive

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -6,19 +6,28 @@ function index() {
   return (
     <div className="index">
       <div>
-        <h1> Index page</h1>
+        <h1> Om mig</h1>
         <p>
           Tja heter sam studerar webbprogrammering på BTH. Detta är min kursida
           för jsramverk kursen på BTH. Jag valde att jobba med react för jag
           tycker det verkar mer spännande och jag har tidigare jobbat med
           angular och använde typescript. Det var lite av det i mitt grupprojekt
           och har även jobbat med angular i lite små projekt vid sidan av
-          skolan. Språk php, js, ruby, typescript, javascript och python. Jag
-          glömmer säkert något det är svårt att komma ihåg allt ibland. När jag
-          inte spenderar min tid på programmering så gillar jag att träna
-          gym/boxning eller ta en runda med hundarna. HEJ SÅ LÄNGE VI HÖRS I
-          GITTER CHATTEN! RIP HASSE RAVELI.
+          skolan.
         </p>
+        <p>
+          Språk php, js, ruby, typescript, javascript och python. Jag glömmer
+          säkert något det är svårt att komma ihåg allt ibland.
+        </p>
+
+        <p>
+          När jag inte spenderar min tid på programmering så gillar jag att
+          träna gym/boxning eller ta en runda med hundarna.
+        </p>
+
+        <p>HEJ SÅ LÄNGE VI HÖRS I GITTER CHATTEN! </p>
+
+        <p>RIP HASSE RAVELI.</p>
       </div>
       <div className="image"></div>
     </div>
